7400941bec703af3703cdd60a24fd04bf9c8f058
KeBiem <37881314+KeBiem@users.noreply.github.com>
2018-05-09 20:48:35 +0200

    Add files via upload

---
+++ cmds/rps.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const rps = ["rock", "paper", "scissors"];
+function rand(low, high) {
+return Math.random() * (high + 1 - low) + low | 0;
+}
+
+module.exports.run = async (bot, message, args) => {
+
+  if (args[0]) {
+    // get user choice && user choice
+    let computer_choice = rand(0,2);
+    let user_choice = args[0] == "rock" ? 1 : args[0] == "paper" ? 2 : 0;
+
+    // if their choices are same its a draw :D
+    if (computer_choice == user_choice) {
+      message.reply("Game Drawn!");
+    }
+    else if (computer_choice < user_choice || computer_choice == 0 && user_choice == 2) {
+      message.reply("Tirebolu Won!");
+    } else {
+      message.reply("You Won!");
+    }
+  }
+}
+
+module.exports.help = {
+    name: "rps"
+}
