250756e683d342dfbb1540f2f72a2cb6ad9267c3
KeBiem <37881314+KeBiem@users.noreply.github.com>
2018-05-11 23:59:14 +0200

    Add files via upload

--- cmds/gif.js
+++ cmds/gif.js
@@ -3,6 +3,8 @@ const gifSearch = require("gif-search");
 
 module.exports.run = async (bot, message, args) => {
 
+  let jsicon = "https://discord.js.org/static/logo-square.png";
+
   if (message.author.bot) return;
 
     if (!args[0]) return message.channel.send("-gif <gname>`");
@@ -13,7 +15,9 @@ module.exports.run = async (bot, message, args) => {
         let randomcolor = ((1 << 24) * Math.random() | 0).toString(16); //Optional
         var embed = new Discord.RichEmbed()
             .setColor(`#${randomcolor}`)
-            .setImage(gifUrl);
+            .setImage(gifUrl)
+            .setTimestamp()
+            .setFooter("Tirebolu ©2018", jsicon);
         message.channel.send(embed);
      });
 }
