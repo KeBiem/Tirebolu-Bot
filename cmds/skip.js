9d5229c02561da2957bdff4cc1a6648ca7d4bee8
KeBiem <37881314+KeBiem@users.noreply.github.com>
2018-05-23 08:59:08 +0200

    Create skip.js

---
+++ cmds/skip.js
@@ -0,0 +1,22 @@
+//skip
+const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
+function play(connection, message) {
+    var server = servers[message.guild.id];
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.queue.shift();
+    server.dispatcher.on("end", function() {
+        if(server.queue[0]) play(connection, messsage);
+        else connection.disconnect();
+    })
+}
+var servers = {};
+module.exports.run = async (bot, message, args) => {
+    //skip
+    var server = servers[message.guild.id];
+    if (server.dispatcher) server.dispatcher.end();
+}
+
+module.exports.help = {
+    name: "skip"
+}
